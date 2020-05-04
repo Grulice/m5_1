477cba7a802d58c0d98f483168427f31023c410e
Javid Bunyadzade <j.bunyadzade@gmail.com>
2020-05-04 14:25:00 +0400

    Sell bugfixes + minor refactor

--- src/sell/Sell.jsx
+++ src/sell/Sell.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {getStockPriceFor,changeUserStockDelete,changeUserStockPutExport,updateBalance,getUserBalance} from "./sellFetcher";
+import {
+  getStockPriceFor,
+  changeUserStockDelete,
+  changeUserStockPutExport,
+  updateBalance,
+  getUserBalance,
+} from "./sellFetcher";
 
 import {
-    MainSell,
-    CentralBlock,
-    TestBlock,
-    HeaderSell,
-    PriceText,
-    SellFor,
-    InputBlock,
-    InputLenght,
+  MainSell,
+  CentralBlock,
+  TestBlock,
+  HeaderSell,
+  PriceText,
+  SellFor,
+  InputBlock,
+  InputLenght,
 } from "./styleSell";
 import styled from "styled-components";
 import arrow from "../img/arrow.svg";
@@ -19,119 +25,154 @@ import arrow from "../img/arrow.svg";
 // Стили Компонента Chart Конец ****
 
 class Buy extends React.Component {
-    state = {
-        name: null,
-        symbol: null,
-        amount:null,
-        currentPrice:null,
-        id:null,
-        currentBalance:null,
-        oldPrice:null,
-        pieces: "",
+  state = {
+    name: null,
+    symbol: null,
+    amount: null,
+    currentPrice: null,
+    id: null,
+    currentBalance: null,
+    oldPrice: null,
+    pieces: "",
+  };
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    const {
+      stockTicker,
+      stockName,
+      stockAmount,
+      stockId,
+      oldPrice,
+    } = this.props.location.state; //Беру state из тега Link в компоненте Stock
+    this.setState({
+      name: stockName,
+      symbol: stockTicker,
+      amount: stockAmount,
+      id: stockId,
+      oldPrice: oldPrice,
+    });
+    getStockPriceFor(stockTicker).then((res) =>
+      this.setState({ currentPrice: res.profile.price })
+    );
+    getUserBalance().then((res) =>
+      this.setState({ currentBalance: res.currentBalance })
+    );
+  }
+  // Функция выделяющая числа после точки для ее уменьшения в стилях в дальнейшем
+  numberAfterDot = (value) => {
+    if (value) {
+      value.toFixed(2);
+      value = value + "";
+      const digits = value.substring(value.indexOf(".") + 1);
+      return "." + digits.substring(0, 2);
+    } else return null;
+  };
+  // Функция увеличения значения в input
+  handlerPlus = () => {
+    if (this.state.pieces >= this.state.amount)
+      this.setState({ pieces: this.state.amount });
+    else this.setState({ pieces: +this.state.pieces + 1 });
+  };
+  // Функция уменьшения значения в input
+  handlerMinus = () => {
+    if (this.state.pieces <= 0) this.setState({ pieces: 0 });
+    else this.setState({ pieces: +this.state.pieces - 1 });
+  };
 
-    };
-    componentDidMount() {
-        window.scrollTo(0, 0);
-        const { stockTicker, stockName, stockAmount,stockId,oldPrice } = this.props.location.state; //Беру state из тега Link в компоненте Stock
-        this.setState({
-            name: stockName,
-            symbol: stockTicker,
-            amount:stockAmount,
-            id:stockId,
-            oldPrice:oldPrice
-        });
-        getStockPriceFor(stockTicker).then(
-            res => this.setState({currentPrice:res.profile.price})
-        );
-        getUserBalance().then(res => this.setState({currentBalance:res.currentBalance}))
+  sendStocksInfo = () => {
+    if (this.state.pieces <= 0 || this.state.pieces > this.state.amount) {
+      alert("Неверное количество акций");
+      return;
     }
-    // Функция выделяющая числа после точки для ее уменьшения в стилях в дальнейшем
-    numberAfterDot = (value) => {
-        if (value) {
-            value.toFixed(2);
-            value = value + "";
-            const digits = value.substring(value.indexOf(".") + 1);
-            return "." + digits.substring(0, 2);
-        } else return null;
-    };
-    // Функция увеличения значения в input
-    handlerPlus = () => {
-        if(this.state.pieces >= this.state.amount)  this.setState({ pieces:this.state.amount});
-        else  this.setState({ pieces: +this.state.pieces + 1 });
-    };
-    // Функция уменьшения значения в input
-    handlerMinus = () => {
-        if (this.state.pieces <= 0) this.setState({ pieces: 0 });
-        else this.setState({ pieces: +this.state.pieces - 1 });
-    };
+    if (this.state.pieces === this.state.amount) {
+      changeUserStockDelete(this.state.id);
+      const element =
+        +this.state.currentBalance +
+        this.state.pieces * this.state.currentPrice;
+      updateBalance(element).then(() => this.props.refreshBalance());
+    } else {
+      const obj = {
+        id: this.state.id,
+        amount: this.state.amount - this.state.pieces,
+        price: (this.state.amount - this.state.pieces) * this.state.oldPrice,
+      };
+      const element =
+        +this.state.currentBalance +
+        this.state.pieces * this.state.currentPrice;
+      changeUserStockPutExport(obj);
+      updateBalance(element).then(() => this.props.refreshBalance());
+    }
+  };
 
-    sendStocksInfo = () =>{
-        if(this.state.pieces===this.state.amount){
-            changeUserStockDelete(this.state.id);
-            const element = +this.state.currentBalance + (this.state.pieces * this.state.currentPrice);
-            updateBalance(element);
-        }
-        else{
-            const obj = {
-                id:this.state.id,
-                amount:this.state.amount -  this.state.pieces,
-                price: (this.state.amount -  this.state.pieces) * this.state.oldPrice
-            };
-            const element = +this.state.currentBalance + (this.state.pieces * this.state.currentPrice);
-            updateBalance(element);
-            changeUserStockPutExport(obj);
-        }
-    };
-
-    // Функция записывающая текущее значение value input  в state pieces
-    changeValue = (e) => {
-        this.setState({ pieces: e.target.value });
-        if (e.target.value.length === 0) e.target.style.width = `100px`;
-        else {
-            e.target.style.width = (e.target.value.length + 20) * 8 + "px"; // Динамическое расширение и уменьшения поля input в зависимости от введенного значения
-        }
-        parseInt(e.target.value);
-    };
-    render() {
-        return (
-            <TestBlock>
-                <MainSell>
-                    <HeaderSell>
-                        <Link to={"/Account"}>
-                            <img src={arrow} alt="arrow" />
-                            Back
-                        </Link>
-                        <h2>Sell {this.state.name}</h2>
-                    </HeaderSell>
-                    <CentralBlock>
-                        <PriceText>
-                            {Math.trunc(this.state.currentPrice)}
-                            <span>{this.numberAfterDot(this.state.currentPrice)} $</span>
-                        </PriceText>
-                        <InputBlock>
-                            <button onClick={this.handlerMinus}>-</button>
-                            <InputLenght
-                                type="number"
-                                min="0"
-                                onChange={this.changeValue}
-                                value={this.state.pieces}
-                                placeholder="0"
-                            />
-                            <button onClick={this.handlerPlus}>+</button>
-                        </InputBlock>
-                        <SellFor>
-                            Sell for {Math.trunc(this.state.pieces * this.state.currentPrice)}
-                            <span>
-                {this.numberAfterDot(this.state.pieces * this.state.currentPrice)} $
+  // Функция записывающая текущее значение value input  в state pieces
+  changeValue = (e) => {
+    this.setState({ pieces: e.target.value });
+    if (e.target.value.length === 0) e.target.style.width = `100px`;
+    else {
+      e.target.style.width = (e.target.value.length + 20) * 8 + "px"; // Динамическое расширение и уменьшения поля input в зависимости от введенного значения
+    }
+    parseInt(e.target.value);
+  };
+  render() {
+    return (
+      <TestBlock>
+        <MainSell>
+          <HeaderSell>
+            <Link to={"/Account"}>
+              <img src={arrow} alt="arrow" />
+              Back
+            </Link>
+            <h2>Sell {this.state.name}</h2>
+          </HeaderSell>
+          <CentralBlock>
+            <PriceText>
+              {Math.trunc(this.state.currentPrice)}
+              <span>{this.numberAfterDot(this.state.currentPrice)} $</span>
+            </PriceText>
+            <InputBlock>
+              <button onClick={this.handlerMinus}>-</button>
+              <InputLenght
+                type="number"
+                min="1"
+                max={this.state.amount}
+                onChange={this.changeValue}
+                value={this.state.pieces}
+                placeholder="1"
+              />
+              <button onClick={this.handlerPlus}>+</button>
+            </InputBlock>
+            <SellFor>
+              Sell for {Math.trunc(this.state.pieces * this.state.currentPrice)}
+              <span>
+                {this.numberAfterDot(
+                  this.state.pieces * this.state.currentPrice
+                )}{" "}
+                $
               </span>
-                        </SellFor>
-                        <Link to={'/Account'}>
-                            <p onClick={this.sendStocksInfo}>Sell</p>
-                        </Link>
-                    </CentralBlock>
-                </MainSell>
-            </TestBlock>
-        );
-    }
+            </SellFor>
+            <Link
+              to={{
+                pathname:
+                  this.state.pieces <= 0 ||
+                  this.state.pieces === "" ||
+                  this.state.pieces > this.state.amount
+                    ? "/Sell"
+                    : "/Account",
+                state: {
+                  name: this.state.name,
+                  symbol: this.state.symbol,
+                  amount: this.state.amount,
+                  id: this.state.id,
+                  oldPrice: this.state.oldPrice,
+                },
+              }}
+            >
+              <p onClick={this.sendStocksInfo}>Sell</p>
+            </Link>
+          </CentralBlock>
+        </MainSell>
+      </TestBlock>
+    );
+  }
 }
 export default Buy;
--- src/sell/sellFetcher.js
+++ src/sell/sellFetcher.js
@@ -1,48 +1,49 @@
-export function getStockPriceFor(code){
-        return fetch(`https://financialmodelingprep.com/api/v3/company/profile/${code}`) 
-          .then(res=>res.json());
+export function getStockPriceFor(code) {
+  return fetch(
+    `https://financialmodelingprep.com/api/v3/company/profile/${code}`
+  ).then((res) => res.json());
 }
 export function getUserBalance() {
-    return fetch(
-        "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1"
-    ).then((res) => res.json());
+  return fetch(
+    "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1"
+  ).then((res) => res.json());
 }
 
 export function changeUserStockPutExport(updatedStock) {
-    fetch(
-      "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1/stocks/" + updatedStock.id,
-      {
-        method: "PUT",
-        body: JSON.stringify({
-          amount: updatedStock.amount,
-          purchasePrice:updatedStock.price
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  }
-  export function changeUserStockDelete(id) {
-    fetch(
-        "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1/stocks/" + id,
-        {
-            method: "DELETE",
-        }
-    );
-  }
-
-
-  export function updateBalance(amount){
-    fetch("https://5e8da89e22d8cd0016a798db.mockapi.io/users/1", {
+  return fetch(
+    "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1/stocks/" +
+      updatedStock.id,
+    {
       method: "PUT",
       body: JSON.stringify({
-        id: "1",
-        name: "Team one",
-        currentBalance: amount,
+        amount: updatedStock.amount,
+        purchasePrice: updatedStock.price,
       }),
       headers: {
         "Content-Type": "application/json",
       },
-    });
-  }
+    }
+  );
+}
+export function changeUserStockDelete(id) {
+  return fetch(
+    "https://5e8da89e22d8cd0016a798db.mockapi.io/users/1/stocks/" + id,
+    {
+      method: "DELETE",
+    }
+  );
+}
+
+export function updateBalance(amount) {
+  return fetch("https://5e8da89e22d8cd0016a798db.mockapi.io/users/1", {
+    method: "PUT",
+    body: JSON.stringify({
+      id: "1",
+      name: "Team one",
+      currentBalance: amount,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
